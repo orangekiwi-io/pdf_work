d3ad27d01042605fbc6d02daff55c6f356c80f92
orangekiwi-io <hello@orangekiwi.io>
2024-03-05 12:01:28 +0000

    Initial commit

---
+++ pdf_ok/build.rs
@@ -0,0 +1,8 @@
+//! This is the main function for the build script.
+//!
+//! Currently, it only instructs Cargo to re-run this build script if
+//! `build.rs` is changed.
+fn main() {
+    // Avoid unnecessary re-building.
+    println!("cargo:rerun-if-changed=build.rs");
+}
---
+++ pdf_ok/src/lib.rs
@@ -0,0 +1,24 @@
+#![crate_type = "lib"]
+
+use colored::*;
+use dotenvy::dotenv;
+use std::env;
+
+/// This is the main entry point for `Rust and Cargo starter (RCS)`
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().expect(".env file not found");
+
+    // TEST_MODE value can be changed in the .env file in the root of this project
+    if env::var("TEST_MODE").unwrap_or_default() == "1" {
+        println!("\n{} value: {}", "TEST_MODE".bright_cyan(), env::var("TEST_MODE").unwrap().to_string().bright_green());
+        return Err("Simulated error\n".yellow().into());
+    }
+
+    let name = env::var("PROJECT_NAME").unwrap().to_string();
+    println!("\nWelcome to {}", name.yellow());
+    println!(
+        "PDF creation from Front Matter (YAML) files."
+    );
+
+    Ok(())
+}
